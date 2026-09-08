98fa59e8d5a43cd0fc768da74a6414b0a4db721e
kauanfiolo-04 <kauanfiolo@hotmail.com>
2026-09-08 16:04:49 -0300

    update implementation

--- JDBC/demo-dao-jdbc/src/model/dao/impl/SellerDaoJDBC.java
+++ JDBC/demo-dao-jdbc/src/model/dao/impl/SellerDaoJDBC.java
@@ -52,12 +52,36 @@ public class SellerDaoJDBC implements SellerDao {
           }
         } catch (SQLException e) {
             throw new DbException(e.getMessage());
+        } finally {
+            DB.closeStatement(st);
         }
     }
 
     @Override
     public void update(Seller seller) {
+        PreparedStatement st = null;
 
+        try {
+            st = conn.prepareStatement(
+            "UPDATE seller " +
+                "SET Name = ?, Email = ?, BirthDate = ?, BaseSalary = ? , DepartmentId = ? " +
+                "WHERE Id = ?"
+            , PreparedStatement.RETURN_GENERATED_KEYS);
+
+            st.setString(1, seller.getName());
+            st.setString(2, seller.getEmail());
+            st.setDate(3, new java.sql.Date(seller.getBirthDate().getTime()));
+            st.setDouble(4, seller.getBaseSalary());
+            st.setInt(5, seller.getDepartment().getId());
+
+            st.setInt(6, seller.getId());
+
+            st.executeUpdate();
+        } catch (SQLException e) {
+            throw new DbException(e.getMessage());
+        } finally {
+            DB.closeStatement(st);
+        }
     }
 
     @Override
