79454fb06922747653bff3492fae5909251e8a29
kauanfiolo-04 <kauanfiolo@hotmail.com>
2026-09-08 15:45:40 -0300

    findAll implementation

--- JDBC/demo-dao-jdbc/src/app/Program.java
+++ JDBC/demo-dao-jdbc/src/app/Program.java
@@ -16,8 +16,12 @@ public class Program {
     Seller seller = sellerDao.findById(3);
     System.out.println(seller);
 
-    System.out.println("=== TEST 2: seller findByDepartment ===");
+    System.out.println("\n=== TEST 2: seller findByDepartment ===");
     List<Seller> list = sellerDao.findByDepartment(new Department(2, null));
+    list.forEach(System.out::println);
+
+    System.out.println("\n=== TEST 3: seller findAll ===");
+    list = sellerDao.findAll();
     list.forEach(System.out::println);
   }
 }
--- JDBC/demo-dao-jdbc/src/model/dao/impl/SellerDaoJDBC.java
+++ JDBC/demo-dao-jdbc/src/model/dao/impl/SellerDaoJDBC.java
@@ -94,7 +94,41 @@ public class SellerDaoJDBC implements SellerDao {
 
     @Override
     public List<Seller> findAll() {
-        return List.of();
+        PreparedStatement st = null;
+        ResultSet rs = null;
+
+        try {
+            st = conn.prepareStatement(
+                    "SELECT seller.*,department.Name as DepName " +
+                            "FROM seller INNER JOIN department " +
+                            "ON seller.DepartmentId = department.Id " +
+                            "ORDER BY Name"
+            );
+
+            rs = st.executeQuery();
+
+            List<Seller> sellers = new ArrayList<>();
+            Map<Integer, Department> map = new HashMap<>();
+
+            while (rs.next()) {
+                Department dep = map.get(rs.getInt("DepartmentId"));
+
+                if (dep == null) {
+                    dep = instantiateDepartment(rs);
+                    map.put(rs.getInt("DepartmentId"), dep);
+                }
+
+                Seller seller = instantiateSeller(rs, dep);
+                sellers.add(seller);
+            }
+
+            return sellers;
+        } catch (SQLException e) {
+            throw new DbException(e.getMessage());
+        } finally {
+            DB.closeStatement(st);
+            DB.closeResultSet(rs);
+        }
     }
 
     @Override
@@ -126,7 +160,7 @@ public class SellerDaoJDBC implements SellerDao {
                     map.put(rs.getInt("DepartmentId"), dep);
                 }
 
-                Seller seller = instantiateSeller(rs, dept);
+                Seller seller = instantiateSeller(rs, dep);
                 sellers.add(seller);
             }
 
