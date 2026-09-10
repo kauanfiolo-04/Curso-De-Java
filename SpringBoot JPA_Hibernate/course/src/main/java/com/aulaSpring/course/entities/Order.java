a3e2c5c7c2f4a5b3a9257e872f9bd9dc7301c4f0
kauanfiolo-04 <kauanfiolo@hotmail.com>
2026-09-10 09:42:42 -0300

    Subtotal & Total methods

--- SpringBoot JPA_Hibernate/course/src/main/java/com/aulaSpring/course/entities/Order.java
+++ SpringBoot JPA_Hibernate/course/src/main/java/com/aulaSpring/course/entities/Order.java
@@ -89,6 +89,12 @@ public class Order implements Serializable {
         return items;
     }
 
+    public Double getTotal() {
+        return items.stream()
+            .map(OrderItem::getSubTotal)
+            .reduce(0.0, Double::sum);
+    }
+
     @Override
     public boolean equals(Object o) {
         if (o == null || getClass() != o.getClass()) return false;
