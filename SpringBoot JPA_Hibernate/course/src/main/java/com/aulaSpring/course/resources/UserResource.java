596f2fafff5656ee61cd6c8bd7ee4612937d3046
kauanfiolo-04 <kauanfiolo@hotmail.com>
2026-09-10 10:08:08 -0300

    User update

--- SpringBoot JPA_Hibernate/course/src/main/java/com/aulaSpring/course/resources/UserResource.java
+++ SpringBoot JPA_Hibernate/course/src/main/java/com/aulaSpring/course/resources/UserResource.java
@@ -51,4 +51,14 @@ public class UserResource {
 
         return ResponseEntity.noContent().build();
     }
+
+    @PutMapping(value = "/{id}")
+    public ResponseEntity<User> update(
+        @PathVariable Long id,
+        @RequestBody User user
+    ) {
+        User response = userService.update(id, user);
+
+        return ResponseEntity.ok().body(response);
+    }
 }
