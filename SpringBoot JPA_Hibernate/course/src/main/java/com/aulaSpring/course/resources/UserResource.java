90a48918bcdaaddc999db3fd76e084abf335887d
kauanfiolo-04 <kauanfiolo@hotmail.com>
2026-09-10 09:52:59 -0300

    User insert

--- SpringBoot JPA_Hibernate/course/src/main/java/com/aulaSpring/course/resources/UserResource.java
+++ SpringBoot JPA_Hibernate/course/src/main/java/com/aulaSpring/course/resources/UserResource.java
@@ -5,11 +5,10 @@ import com.aulaSpring.course.entities.User;
 import com.aulaSpring.course.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -32,5 +31,18 @@ public class UserResource {
         User response = userService.findById(id);
 
         return ResponseEntity.ok().body(response);
+    }
+
+    @PostMapping
+    public ResponseEntity<User> insert(@RequestBody User user) {
+        User response = userService.insert(user);
+
+        URI uri =
+            ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(user.getId()).toUri();
+
+        return ResponseEntity.created(uri).body(response);
     }
 }
